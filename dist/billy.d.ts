549e2f3078ae844a50d3f3dcd569aeb99414988e
Gary Großgarten <gary.grossgarten@gmail.com>
2019-01-09 00:48:45 +0100

    install plugin and remove plugin, application generation

--- dist/billy.d.ts
+++ dist/billy.d.ts
@@ -4,4 +4,7 @@ export declare class BillyCLI extends Application {
     create_plugin(): Promise<void>;
     install_plugin(): Promise<void>;
     remove_plugin(): Promise<void>;
+    addPlugin(name: string): void;
+    removePlugin(name: string): void;
+    getContent(imports: string[], currentPlugins: string[]): string;
 }
